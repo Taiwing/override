bab720098cc3f8c0daa67c024e5c28ada9c6791d
yoann foreau <yoann.foreau@ito.am>
2022-07-19 21:01:39 +0200

    docs: beautify source

--- level08/source.c
+++ level08/source.c
@@ -19,29 +19,33 @@ int main(int argc, char **argv)
 	int		fd;
 	FILE	*log_file;
 	FILE	*source_file;
-	// dont know the size here yet (although its not uber important)
 	char	buffer[1000];
 	char	dest[100] = "./backups/";
 
 	if (argc != 2)
 		printf("Usage: %s filename\n", *argv);
+
 	if ((log_file = fopen("./backups/.log", "w")) == NULL) {
 		printf("ERROR: Failed to open %s\n", "./backups/.log");
 		exit(1);
 	}
+
 	log_wrapper(log_file, "Starting back up: ", argv[1]);
 	if ((source_file = fopen(argv[1], "r")) == NULL) {
 		printf("ERROR: Failed to open %s\n", argv[1]);
 		exit(1);
 	}
+
 	strncat(dest, argv[1], sizeof(dest) - strlen(dest) - 1);
 	if ((fd = open(dest, 0xc1, 0x1b0)) < 0) {
 		printf("ERROR: Failed to open %s%s\n", "./backups/", argv[1]);
 		exit(1);
 	}
+
 	while (fgetc(source_file) != -1)
 		write(fd, buffer, 1);
 	log_wrapper(log_file, "Finished back up ", argv[1]);
+
 	fclose(source_file);
 	close(fd);
 	return (0);
